61d28829bec3585f64dde23909a0ec96417cf984
MiniMatash <inkvisitore@gmail.com>
2016-03-08 14:01:30 +0200

    Hibernate v.1

--- src/main/java/com/minimatash/controller/MainPageServlet.java
+++ src/main/java/com/minimatash/controller/MainPageServlet.java
@@ -1,14 +1,23 @@
 package com.minimatash.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/mainPage.html"})
 public class MainPageServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     {
+        try {
+            request.getRequestDispatcher("jsp/mainPage.jsp").forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
